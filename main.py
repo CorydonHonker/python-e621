3851805f3c01c39b2da652822f01b81912f87d34
Honker <35270197+CorydonHonker@users.noreply.github.com>
2021-12-21 11:16:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,12 @@ import e621_downloader as c_mgr
 # from e621_downloader import check_css
 # from tools import TagManager
 
-# check_css()
-
 custom_mgr = c_mgr.GroupManager()
-custom_mgr.post_search("set:cm_favlist_5")  # current set
-# custom_mgr.post_search("set:cm_favlist_4")
-# custom_mgr.post_search("set:cm_favlist_3")
-# custom_mgr.post_search("set:cm_favlist_2")
-# custom_mgr.post_search("set:cm_favlist_1")
-# custom_mgr.post_search("set:cm_favlist_0")
+custom_mgr.post_search("Example_Search")  # current search
 # custom_mgr.family_search()  # broken?
 # custom_mgr.download()
 
-# Relocate is broken
-# my_GUI.start_gui()
+# my_GUI.start_gui()  # not good at gui
 
 # cleaner = c_mgr.CleanSets()
 # cleaner.search('set:cm_favlist_0')
@@ -26,10 +18,5 @@ custom_mgr.post_search("set:cm_favlist_5")  # current set
 # cleaner.clean()
 
 # t_mgr = TagManager()
-# t_mgr.e621_search('set:cm_favlist_5')
-# t_mgr.e621_search('set:cm_favlist_4')
-# t_mgr.e621_search('set:cm_favlist_3')
-# t_mgr.e621_search('set:cm_favlist_2')
-# t_mgr.e621_search('set:cm_favlist_1')
-# t_mgr.e621_search('set:cm_favlist_0')
+# t_mgr.e621_search('Example_Search')
 # t_mgr.make_c_tags()
